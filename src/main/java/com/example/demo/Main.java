1dba874a991874582b15f1ed9fd483e62cc8d035
Juan Pablo ESCAMILLA MONTILLA <juan.escamilla@correounivalle.edu.co>
2025-03-05 18:23:06 -0500

    Start of the FrontEnd development and Main Class creation.
    - Creation of the Main class for the game's operation.
    - Creation of the game's frontend.

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Main {
+    LevelManager levelManager = new LevelManager();
+    LifesManager lifesManager = new LifesManager();
+    TimeManager timeManager = new TimeManager();
+    WordManager wordManager = new WordManager();
+
+    public static void main(String[] args) {}
+
+
+
+}
