c351c4f2715a1f9d4f3dd95fbdf9b05292be31cf
Juan Pablo ESCAMILLA MONTILLA <juan.escamilla@correounivalle.edu.co>
2025-03-08 17:12:36 -0500

    Updated de Controller Class and Added the Lifes (provisional) images

--- src/main/java/com/example/crapgame/GameController.java
+++ src/main/java/com/example/crapgame/GameController.java
@@ -3,7 +3,7 @@ package com.example.crapgame;
 import javafx.fxml.FXML;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.image.Image; // Esta es la que necesitas
 import java.awt.*;
 
 public class GameController {
@@ -27,6 +27,49 @@ public class GameController {
     private ProgressBar timeProgressBar;
     @FXML
     private ImageView lifesImageView;
+
+    private void handleSendWordButtonAction() {
+        if (wordManager.checkWord(wordField.getText())) {
+            correctWrongLabel.setText("Correcto");
+        } else {
+            correctWrongLabel.setText("Incorrecto");
+        }
+    };
+    private void handleLifesImageViewAction() {
+        lifesManager.decreaseLifes();
+        lifesManager.lifesOver();
+    };
+    private void handleTimeProgressBarAction() {
+        timeManager.countdown();
+    }
+    private void handleLifesImageChanger() {
+        switch (lifesManager.getLifes()) {
+            case 0:
+                Image imageZeroLifes = new Image(getClass().getResourceAsStream("/com/example/crapgame/lifes/0_lifes.png"));
+                lifesImageView.setImage(imageZeroLifes);
+                break;
+            case 1:
+                Image imageOneLifes = new Image(getClass().getResourceAsStream("/com/example/crapgame/lifes/1_lifes.png"));
+                lifesImageView.setImage(imageOneLifes);
+                break;
+            case 2:
+                Image imageTwoLifes = new Image(getClass().getResourceAsStream("/com/example/crapgame/lifes/2_lifes.png"));
+                lifesImageView.setImage(imageTwoLifes);
+                break;
+            case 3:
+                Image imageThreeLifes = new Image(getClass().getResourceAsStream("/com/example/crapgame/lifes/3_lifes.png"));
+                lifesImageView.setImage(imageThreeLifes);
+                break;
+            case 4:
+                Image imageFourLifes = new Image(getClass().getResourceAsStream("/com/example/crapgame/lifes/4_lifes.png"));
+                lifesImageView.setImage(imageFourLifes);
+                break;
+            default:
+                break;
+        }
+
+    }
+
 
 
 
